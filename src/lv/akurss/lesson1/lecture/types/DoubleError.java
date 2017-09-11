235c9c510c79823525115dfae50fca7194a216b7
Aleksandrs Kartisevs <aleksandrs.kartisevs@ideaportriga.lv>
2017-09-11 22:33:38 +0300

    precision error

---
+++ src/lv/akurss/lesson1/lecture/types/DoubleError.java
@@ -0,0 +1,34 @@
+package lv.akurss.lesson1.lecture.types;
+
+public class DoubleError {
+	public static void main(String[] args) {
+		double a = 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+	}
+}
