ae22bfabb07f278361c32b8e64416b0b8a8d9826
Aleksandrs Kartisevs <aleksandrs.kartisevs@ideaportriga.lv>
2017-09-17 11:56:15 +0300

    task

--- src/lv/akurss/lesson1/lecture/satellite/Main.java
+++ src/lv/akurss/lesson1/lecture/satellite/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int radius = 35786 + 6378;
-		double length = 2 * 3.14 * radius;
+		double length= 2 * 3.14 * radius;
 		double speedPerHour = length / 24;
 		double speedPerSecond = speedPerHour / 3600;
 		
