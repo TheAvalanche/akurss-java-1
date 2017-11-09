4d0df57269b048e287d52e37f07dadcdc47ffc2d
Aleksandrs Kartisevs <aleksandrs.kartisevs@ideaportriga.lv>
2017-11-09 19:56:05 +0200

    lesson 9 homework

---
+++ src/lv/akurss/lesson10/lecture/homework_answers/Expense.java
@@ -0,0 +1,53 @@
+package lv.akurss.lesson10.lecture.homework_answers;
+
+import java.math.BigDecimal;
+
+public class Expense implements Comparable<Expense> {
+	private BigDecimal amount;
+	private String name;
+	private Category category;
+
+	public Expense(BigDecimal amount, String name, Category category) {
+		this.amount = amount;
+		this.name = name;
+		this.category = category;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int compareTo(Expense o) {
+		return this.getAmount().compareTo(o.getAmount());
+	}
+
+	@Override
+	public String toString() {
+		return "Expense{" +
+				"amount=" + amount +
+				", name='" + name + '\'' +
+				", category=" + category +
+				'}';
+	}
+}
